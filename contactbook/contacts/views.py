dec3b13009c20ea9e0e8f9389378edec72a71734
prozack <zackbrown27@gmail.com>
2016-05-03 12:16:00 -0600

    Added index.html

--- contactbook/contacts/views.py
+++ contactbook/contacts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from .models import Contact
 
 # Create your views here.
+def index(request):
+    context = {
+        'heading': 'Welcome to A Contact Book',
+        'content': 'Must <a href="/login/">login</a> to use the application.',
+        'title': 'A Contact Book'
+    }
+    #return HttpResponse(html)
+    return render(request, 'contacts/index.html', context)
